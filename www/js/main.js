e5bd9fb23b63063151ddac2a5e2c348fa3283537
Maruti <marutitathe@gmail.com>
2018-05-18 19:45:43 +0530

    Initial commit

---
+++ www/js/main.js
@@ -0,0 +1,60 @@
+var screenHeight = 0;
+var screenWidth = 0;
+var canvas;
+function getsetScreenSize() {
+    screenWidth = window.screen.width;
+    screenHeight = window.screen.height;
+
+    canvas = document.getElementById("mainCanvas");
+    canvas.style.height = screenHeight + "px";
+
+    var canvasBody = canvas.querySelector(".canvasBody");
+    //canvasBody.style.height = (screenHeight - 50 - 40) + "px"; //Remove TitleHeight and FooterHeight from body height
+}
+
+function goPage(pageName) {
+    alert(pageName)
+    window.location.href = pageName;
+}
+
+function logStep(str,isNew)
+{
+    if (isNew == 1)
+    {
+        localStorage.clear();
+        localStorage.setItem("logStep", "DateTime : " + getCurrentDateTime()) + "... \r\n";
+    }
+    str = localStorage.getItem("logStep") + "... \r\n" + str;
+    localStorage.setItem("logStep", str);
+}
+
+function getCurrentDateTime()
+{
+    d = new Date();
+    return d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() + ":" + d.getMilliseconds();
+}
+
+function getTimeStamp(VYear, VMonth, VDate)
+{
+    var d = new Date(VYear, VMonth, VDate);
+    return d.getTime();
+}
+
+function getDateFromTimeStamp(ts)
+{
+    var d = new Date(ts);
+    return d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear();
+}
+
+function getMonthFromTimeStamp(ts)
+{
+    var d = new Date(ts);
+    return d.getMonth();
+}
+
+function getItemDateMonth(ts)
+{
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Augu", "Sep", "Oct", "Nov", "Dec"];
+    var d = new Date(ts);
+    return monthNames[d.getMonth()] + " " + d.getFullYear();
+}
